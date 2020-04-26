69c5adf750d0cd3d8221ece0c9795bc05507bdff
Clark Snowdall <snowdall@mac.com>
2020-04-25 19:23:15 -0700

    Flushed out the IMEI decode function.  Also put in place the nominal case unit test.  Will need to flush out mnore of the tests shortly.

--- internal/imei/imei.go
+++ internal/imei/imei.go
@@ -27,5 +27,54 @@ var (
 // Decode does NOT allocate under any condition. Additionally, it panics if b
 // isn't at least 15 bytes long.
 func Decode(b []byte) (code uint64, err error) {
-	panic(common.ErrNotImplemented)
+
+  // Declare a couple place holders
+  var result uint64 = 0
+  var sum uint8 = 0
+
+  // Check that the incoming slice is exactly 15 bytes
+  // NOTE: Since the challenge instructions mentioned "IMEI" and
+  // not specifically "IMEISV" I made the express decision to assume
+  // that the optionally longer 17 byte IMEISV was not being used.
+  // This of course was a conscious choice to make an interrpretation of
+  // the challenge instructions ... my the coding gods have mercy upon me.
+  if len(b) != 15 {
+    common.Err(ErrInvalid)
+    panic(ErrInvalid)
+  }
+
+  // Cycle through the byte slice
+  for i := 0; i < 15; i++ {
+    // If the index is even (meaning odd digit) just add to summ
+    if i % 2 == 0 {
+      sum = sum + uint8(b[i])
+
+    // Otherwise double the byte
+    } else {
+      double := 2 * uint8(b[i])
+
+      // Check to see if the double is over 9.  If so then take 1
+      // plus the modulo of 10 (assumes largest double would be 18
+      // therefore add the "1")
+      if double > 9 {
+        sum = sum + 1 + (double % 10)
+      } else {
+        sum = sum + double
+      }
+    }
+  }
+
+  // If the sum modulo 10 is 0 then it's valid.  At which point
+  // cycle through the bytes and build it into the final uint64
+  // for return.  Otherwise print the err and panic.
+  if sum % 10 == 0 {
+    for i := 0; i < 15; i++ {
+      result = result * uint64(10) + uint64(b[i])
+    }
+  } else {
+    common.Err(ErrChecksum)
+    panic(ErrChecksum)
+  }
+
+  return result, nil
 }
--- internal/imei/imei_test.go
+++ internal/imei/imei_test.go
@@ -2,14 +2,28 @@ package imei
 
 import (
 	"testing"
-
-	"github.com/spin-org/thermomatic/internal/common"
+	//"github.com/spin-org/thermomatic/internal/common"
 )
 
 func TestDecode(t *testing.T) {
-	panic(common.ErrNotImplemented)
+  // example valid IMEI number 490154203237518
+  var imei = [...]byte { 4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8}
+
+  // Ask for the decode and check the err
+  result, err := Decode(imei[:])
+
+  if err != nil {
+    t.Errorf("Error in calling decode")
+  }
+
+  // Should be the uint64 vcalue of the IMEI
+  if result != 490154203237518 {
+    t.Errorf("Did not receive the proper decoded IMEI")
+  }
+
 }
 
+/*
 func TestDecodeAllocations(t *testing.T) {
 	panic(common.ErrNotImplemented)
 }
@@ -21,3 +35,4 @@ func TestDecodePanics(t *testing.T) {
 func BenchmarkDecode(b *testing.B) {
 	panic(common.ErrNotImplemented)
 }
+*/
